d6f625d227b095bf7e6b631d20dc7558000e3fb3
MSI <wydrasz3@gmail.com>
2020-02-09 13:38:10 +0100

    spring-jdbc

---
+++ src/main/java/pl/sda/config/AppConfig.java
@@ -0,0 +1,21 @@
+package pl.sda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("pl.sda.customer")
+public class AppConfig {
+
+    @Bean
+    public DriverManagerDataSource dataSource(){
+        DriverManagerDataSource ret = new DriverManagerDataSource();
+        ret.setDriverClassName("org.h2.Driver");
+        ret.setUsername("sa");
+        ret.setPassword("");
+        ret.setUrl("jdbc:h2:tcp://localhost/~/test");
+        return ret;
+    }
+}
