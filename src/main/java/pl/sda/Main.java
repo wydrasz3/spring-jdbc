5113e46219052ab2b77cd895a10f6fbe9ec37196
MSI <wydrasz3@gmail.com>
2020-02-09 14:07:45 +0100

    spring-jdbc

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -3,10 +3,16 @@ package pl.sda;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pl.sda.config.AppConfig;
+import pl.sda.customer.CustomerDaoImpl;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        CustomerDaoImpl customerDao = context.getBean(CustomerDaoImpl.class);
+        System.out.println(Arrays.toString(customerDao.findAll().toArray()));
     }
 }
--- src/main/java/pl/sda/customer/CustomerDaoImpl.java
+++ src/main/java/pl/sda/customer/CustomerDaoImpl.java
@@ -1,14 +1,28 @@
 package pl.sda.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 import java.util.List;
 
 @Component
-public class CustomerDaoImpl implements CustomerDao {
+public class CustomerDaoImpl extends JdbcDaoSupport implements CustomerDao {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void init() {
+        setDataSource(dataSource);
+    }
 
     @Override
     public List<Customer> findAll() {
-        return null;
+        String sql = "SELECT * FROM CUSTOMER";
+        return getJdbcTemplate().query(sql, new BeanPropertyRowMapper(Customer.class));
     }
 }
